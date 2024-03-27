40ca1da43ae3638edd272c2cd8ec9e79a72b66b0
BhanukaAGH <abewickramah1@gmail.com>
2024-03-27 11:09:00 +0530

    build: fixed build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, passthroughImageService } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 import icon from 'astro-icon'
 import markdoc from '@astrojs/markdoc'
 import keystatic from '@keystatic/astro'
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), icon(), markdoc(), keystatic()],
   output: 'hybrid',
+  adapter: node({
+    mode: 'standalone',
+  }),
+  image: {
+    service: passthroughImageService(),
+  },
 })
