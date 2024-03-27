de20ae6681f0ade222a38e529d3efd9a30866cab
BhanukaAGH <abewickramah1@gmail.com>
2024-03-27 11:18:40 +0530

    chore: changed ssr adapter to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,13 @@ import react from '@astrojs/react'
 import icon from 'astro-icon'
 import markdoc from '@astrojs/markdoc'
 import keystatic from '@keystatic/astro'
-import node from '@astrojs/node'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), icon(), markdoc(), keystatic()],
   output: 'hybrid',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   image: {
     service: passthroughImageService(),
   },
